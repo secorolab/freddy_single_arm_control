40c8fd1d72aaad3fe8bb697411d2f64369dd809c
Kishan Sawant <40161007+KishanSawant@users.noreply.github.com>
2025-06-17 17:36:37 +0200

    Revert "Pr"

--- build/motion_specification_interfaces/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(motion_specification_interfaces_IDL_FILES "action/MotionSpecification.idl")
-set(motion_specification_interfaces_INTERFACE_FILES "action/MotionSpecification.action")
